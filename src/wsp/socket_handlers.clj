77882b9b36e7ec4b4f86207388cc9089cfb2c88b
Dan Peddle <dan@flarework.com>
2020-06-25 19:12:11 +0100

    client record for holding a ws connection and an id, provide an id to new conections, persist on client

--- src/wsp/socket_handlers.clj
+++ src/wsp/socket_handlers.clj
@@ -1,21 +1,29 @@
 (ns wsp.socket-handlers
-  (:require [ring.adapter.jetty9 :as j9]))
+  (:require [ring.adapter.jetty9 :as j9])
+  (:import (java.util UUID)))
 
-(def connected-clients (atom #{}))
+(defonce connected-clients (atom {}))
+
+(defrecord Client [ws id])
 
 (defn on-connect [ws]
-  (swap! connected-clients conj ws)
-  (prn ::on-connect ws))
+  (let [client (->Client ws (UUID/randomUUID))]
+    (swap! connected-clients assoc ws client)
+    (j9/send! ws (str "you are - " (:id client)))
+    (prn ::on-connect ws)))
 
 (defn on-error [ws e]
+  ;; on-close is also called
   (prn ::err e))
 
 (defn on-close [ws status-code reason]
-  (swap! connected-clients disj ws)
+  (swap! connected-clients dissoc ws)
   (prn ::clos status-code reason))
 
 (defn on-text [ws text-message]
-  (prn ::ot text-message)
+  (let [{:keys [ws id] :as thing} (get @connected-clients ws)]
+    (prn ws)
+    (prn (= (str id) (str text-message))))
   (try
     (j9/send! ws (str text-message))
     (catch Throwable err
@@ -25,5 +33,6 @@
   (prn ::ob (slurp bytes) offset len))
 
 (comment
-  (doseq [ws @connected-clients]
-    (prn (on-text ws (System/currentTimeMillis)))))
+  (deref connected-clients)
+  (doseq [[ws {:keys [ws id]}] @connected-clients]
+    (on-text ws (System/currentTimeMillis))))
